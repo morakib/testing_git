7d0984868f38766b5facefea9589e0ece0261936
morakib <morakibnashit@gmail.com>
2025-07-20 16:28:32 +0600

    .cpp and .py are updated

---
+++ a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is a C++ program." << std::endl;
+    std::cout << "Let's add another line." << std::endl;
+    return 0;
+}
